d1ef13d17ee4c7a15666aef35bd35e6cf6b28bdc
AlexR0v <mail.ponomarev@yandex.ru>
2020-12-21 01:16:52 +0300

    add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-form';
+export class AppComponent implements OnInit {
+  form: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      address: new FormGroup({
+        country: new FormControl('ru'),
+        city: new FormControl('', Validators.required)
+      }),
+      skills: new FormArray([])
+    })
+  }
+
+  enterCity(value){
+    console.log(value)
+  }
+
+  submit() {
+    const formData = {...this.form.value}
+    console.log(formData)
+    this.form.reset()
+  }
+
+  setCapital(){
+    const cityMap = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск'
+    }
+    const cityKey = this.form.get('address').get('country').value
+    const city = cityMap[cityKey]
+    this.form.patchValue({
+      address: {city}
+    })
+  }
+
+  addSkill() {
+    const control = new FormControl('', Validators.required);
+    (<FormArray>this.form.get('skills')).push(control)
+  }
 }
